14fc1cbbde3a6a63c25c4afcc6c8038916f2eef5
thiagovb46@gmail.com <thiagovb46@gmail.com>
2022-02-05 20:13:33 -0300

    Insert Transaction Form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 interface NewTransactionModalProps{
     isOpen:boolean;
     onRequestClose: () => void;
@@ -8,8 +9,31 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 {
     return (
          
-        <Modal isOpen= {isOpen} onRequestClose = {onRequestClose}>
-        <h2>Cadastrar Transação</h2>
+        <Modal isOpen= {isOpen} onRequestClose = {onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+            <Container>
+                <h2>Cadastrar Transação</h2>
+                <input 
+                    placeholder='Titulo'
+                />
+
+                <input 
+                    type="number"
+                    placeholder="Valor"
+                />
+
+                <input 
+                    placeholder = "Categoria"
+                />
+
+                <button
+                    type = "submit"
+                >
+                    Cadastrar
+                </button>  
+            </Container>
      </Modal>
 );
 }
